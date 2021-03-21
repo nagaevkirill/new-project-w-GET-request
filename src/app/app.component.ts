8202b767267f17ba0eccdc449f271d0bed80ec33
unknown <bmxrider@mail.ru>
2021-03-21 12:15:25 +0300

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'my-new-angular-app';
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit(){
+    console.log('start get req');
+    this.http.get('http://localhost:3000/movies')
+    .subscribe(data => {
+      console.log(data);
+    })
+  }
+
+  
+}
